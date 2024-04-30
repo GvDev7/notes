f706dd87dc9bfa13de8c33a84fe467784713e5d0
Aaron <a.genevision14@gmail.com>
2024-04-30 02:16:35 -0500

    Developed and implemented Form for login and registration page, completed login and register component functionality, frontend and backend communicating successfully

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,10 +2,44 @@ import { useState } from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
 
 const Form = ({ route, method }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const formHeader = method === "login" ? "Login" : "Register";
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            const res = await api.post(route, { username, password })
+            if(method === 'login') {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/")
+            } else {
+                navigate("/login")
+            }
+        }
+        catch (error) {
+            alert(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
   return (
-    <div>Form</div>
+    <form onSubmit={handleSubmit} className="form-container">
+        <h1>{formHeader}</h1>
+        <input className="form-input" value={username} type="text" onChange={(e) => setUsername(e.target.value)} placeholder="Username"/>
+        <input className="form-input" value={password} type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button className="form-btn" type="submit">{formHeader}</button>
+    </form>
   )
 }
 
